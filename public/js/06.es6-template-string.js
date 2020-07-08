1aaed1a9546094226bf2dbef71e0df0cff720433
booldook <booldook@gmail.com>
2020-07-08 14:44:29 +0900

    template string

--- public/js/06.es6-template-string.js
+++ public/js/06.es6-template-string.js
@@ -4,7 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if(this.readyState == 4 && this.status == 200) {
 		const res = JSON.parse(this.responseText);
-		let html = '';
+		let html = '', remain_txt = '';
 		for(var v of res.stores) {
 			// html += '<tr>';
 			// html += '<td>' + v.code + '</td>';
@@ -12,13 +12,18 @@ xhttp.onreadystatechange = function() {
 			// html += '<td>' + v.addr + '</td>';
 			// html += '<td>' + v.remain_stat + '</td>';
 			// html += '</tr>';
+			if(v.remain_stat == "empty" || v.remain_stat == "break") remain_txt = '없음';
+			else if(v.remain_stat == "few") remain_txt = '소량';
+			else if(v.remain_stat == "some") remain_txt = '적당';
+			else if(v.remain_stat == "plenty") remain_txt = '충분';
 			html += `
-			<tr>
-				<td>${v.code}</td>
-				<td>${v.name}</td>
-				<td>${v.addr}</td>
-				<td>${v.remain_stat}</td>
-			</tr>`;
+				<tr>
+					<td>${v.code}</td>
+					<td>${v.name}</td>
+					<td class="text-left">${v.addr}</td>
+					<td>${remain_txt}</td>
+				</tr>
+			`;
 		}
 		document.querySelector(".mask-table tbody").innerHTML = html;
 	}
